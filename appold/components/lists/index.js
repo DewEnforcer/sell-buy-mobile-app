11d81c5ec84343daae39934a8f560be919de0903
DewEnforcer <piranha863@gmail.com>
2021-01-30 22:02:42 +0100

    Removed junk files

--- appold/components/lists/index.js
+++
@@ -1,3 +0,0 @@
-export {default as ListItem} from "./ListItem";
-export {default as ListItemDeleteAction} from "./ListItemDeleteAction";
-export {default as ListItemSeperator} from "./ListItemSeperator";
